35ab6074454b70a0e06aa11886eb4dcfbfe37b7c
subin <psb2638@naver.com>
2023-05-19 01:06:13 +0900

    first commit

---
+++ src/Page/PageHeader.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const PageHeader = () => {
+  return (
+    <Title>안녕하세용?</Title>
+  )
+}
+
+const Title = styled.h1`
+  color: red;
+  font-size: 24px;
+`
